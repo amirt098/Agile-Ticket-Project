a2b2c5bcfc9868ec893c7e3979f47ff8045087cc
amirt098 <amir0tohidi@gmail.com>
2023-12-19 00:41:45 +0330

    ADD AND TEST ADMIN FOR ACCOUNT!

--- AgileTicketProject/accounts/admin.py
+++ AgileTicketProject/accounts/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User, Organization
 
-# Register your models here.
+class UserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'is_agent', 'organization')
+    fieldsets = (
+        (None, {'fields': ('username', 'password', 'is_agent')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'email')}),
+        ('Organization', {'fields': ('organization',)}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2'),
+        }),
+    )
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Organization)
+
+
